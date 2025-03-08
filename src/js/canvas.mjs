c3094304556a37e26292cca19038b7d005fb0815
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-03-08 17:13:23 -0600

    move canvas code to its own module

---
+++ src/js/canvas.mjs
@@ -0,0 +1,66 @@
+/**
+ * canvas.mjs
+ */
+import lessons from "./lessons.mjs"
+
+// canvas object
+const cvs = {
+	// canvas html element
+	canvas: undefined,
+	// canvas width/height
+	width: undefined,
+	height: undefined,
+	// canvas rendering context
+	ctx: undefined
+}
+
+function setCanvasDimensions() {
+	// set widths to window
+	cvs.width = (cvs.canvas.width = window.innerWidth)
+	cvs.height = (cvs.canvas.height = window.innerHeight)
+}
+
+function clearCanvas() {
+	cvs.ctx.fillStyle = 'rgb(0 0 0)'
+	cvs.ctx.fillRect(0, 0, cvs.width, cvs.height)
+}
+
+const canvas = {
+	// adjust canvas after window resize
+	adjustCanvas: function () {
+		// set new dimensions
+		setCanvasDimensions()
+	},
+
+	// check browser for canvas support
+	isCanvasSupported: function () {
+		if (cvs.canvas.getContext) {
+			return true
+		}
+		return false
+	},
+
+	// configure canvas object
+	configCanvas: function () {
+		// get canvas
+		cvs.canvas = document.querySelector('#tutorial-canvas')
+		// set canvas dimentions
+		setCanvasDimensions()
+		// get canvas context
+		cvs.ctx = cvs.canvas.getContext('2d')
+	},
+
+	canvasPallete: function () {
+		// clear canvas
+		clearCanvas()
+		// draw menu enabled lessons
+		if (lessons.simpleExample) {
+			lessons.drawSimpleExample(cvs)
+		}
+		if (lessons.rectangularShape) {
+			lessons.drawRectangularShape(cvs)
+		}
+	}
+}
+
+export default canvas
--- src/js/script.js
+++ src/js/script.js
@@ -4,54 +4,11 @@
 import menu from './menu.mjs'
 import { log } from './util.mjs'
 import lessonsSetup from './lessonsSetup.mjs'
-import lessons from './lessons.mjs'
+import canvas from './canvas.mjs'
 
 // script.js
 (function () {
     'use strict'
-
-    // canvas object
-    const cvs = {
-        // canvas html element
-        canvas: undefined,
-        // canvas width/height
-        width: undefined,
-        height: undefined,
-        // canvas rendering context
-        ctx: undefined
-    }
-
-    function setCanvasDimensions() {
-        // set widths to window
-        cvs.width = (cvs.canvas.width = window.innerWidth)
-        cvs.height = (cvs.canvas.height = window.innerHeight)
-    }
-
-    // configure canvas object
-    function configCanvas() {
-        // get canvas
-        cvs.canvas = document.querySelector('#tutorial-canvas')
-        // set canvas dimentions
-        setCanvasDimensions()
-        // get canvas context
-        cvs.ctx = cvs.canvas.getContext('2d')
-    }
-
-    // adjust canvas after window resize
-    function adjustCanvas() {
-        // set new dimensions
-        setCanvasDimensions()
-        // redraw
-        draw()
-    }
-
-    // check browser for canvas support
-    function isCanvasSupported() {
-        if (cvs.canvas.getContext) {
-            return true
-        }
-        return false
-    }
 
     // init lessons
     async function initLessons() {
@@ -59,25 +16,9 @@ import lessons from './lessons.mjs'
         await lessonsSetup.rectangularShapeInit()
     }
 
-    function clearCanvas() {
-        cvs.ctx.fillStyle = 'rgb(0 0 0)'
-        cvs.ctx.fillRect(0, 0, cvs.width, cvs.height)
-    }
-
-    function canvasPallete() {
-        // clear canvas
-        clearCanvas()
-        // draw menu enabled lessons
-        if (lessons.simpleExample) {
-            lessons.drawSimpleExample(cvs)
-        }
-        if (lessons.rectangularShape) {
-            lessons.drawRectangularShape(cvs)
-        }
-    }
-
+    // draw
     function draw() {
-        canvasPallete()
+        canvas.canvasPallete()
         requestAnimationFrame(draw)
     }
 
@@ -86,9 +27,9 @@ import lessons from './lessons.mjs'
         // init menu
         menu.init()
         // configure canvas object
-        configCanvas()
+        canvas.configCanvas()
         // check browser support for canvas
-        if (isCanvasSupported) {
+        if (canvas.isCanvasSupported) {
             log('canvas is supported!')
             // start the lessons
             await initLessons()
@@ -102,6 +43,6 @@ import lessons from './lessons.mjs'
     // run script
     window.onload = canvasTutorial
     // adjust canvas
-    window.onresize = adjustCanvas
+    window.onresize = canvas.adjustCanvas
 
 })();
