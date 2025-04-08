f691d9a55f0e045f661fb3d01c0c7439bf3c7a9a
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-04-07 19:00:36 -0500

    complete transparency lesson

--- src/js/lessons/stylesColorsAndText.mjs
+++ src/js/lessons/stylesColorsAndText.mjs
@@ -46,7 +46,62 @@ const stylesColorsAndText = {
 	},
 
 	drawTransparency: function (cvs) {
+		/**
+		 * transparency is done through globalAlpha property, or assigning semi-transparant colors to stroke/fill style
+		 *
+		 * globalAlpha = transparencyValue
+		 * 	- 0.0 (fully transparent) to 1.0 (fully opaque)
+		 *
+		 * globalAlpha is more useful when drawing a lot of transparent shapes
+		 * otherwise set transparent colors for individual shape
+		 */
 
+		// assigning transparent colors
+		// can use 0.0-1.0 or 0%-100%
+		cvs.ctx.strokeStyle = 'rgb(255 0 0 / 50%)'
+		cvs.ctx.fillStyle = 'rgb(255 0 0 / 50%)'
+
+		// draw four opaque squares with a set of semi transparent circles over them
+		cvs.ctx.fillStyle = 'rgb(200, 100, 100)'
+		cvs.ctx.fillRect(500, 500, 75, 75)
+		cvs.ctx.fillStyle = 'rgb(100, 200, 200)'
+		cvs.ctx.fillRect(575, 500, 75, 75)
+		cvs.ctx.fillStyle = 'rgb(200, 100, 200)'
+		cvs.ctx.fillRect(500, 575, 75, 75)
+		cvs.ctx.fillStyle = 'rgb(250, 150, 150)'
+		cvs.ctx.fillRect(575, 575, 75, 75)
+
+		// set gransparency property
+		cvs.ctx.globalAlpha = 0.2
+
+		// draw semi cricles
+		for (let i = 0; i < 7; i++) {
+			cvs.ctx.beginPath()
+			cvs.ctx.arc(575, 575, 10 + 10 * i, 0, Math.PI * 2, true)
+			cvs.ctx.fill()
+		}
+
+		// reset global alpha
+		cvs.ctx.globalAlpha = 1.0
+
+		// draw four rectangles with increasing opacity
+		cvs.ctx.fillStyle = 'rgb(200, 100, 100)'
+		cvs.ctx.fillRect(569, 14, 150, 37.5)
+		cvs.ctx.fillStyle = 'rgb(100, 200, 200)'
+		cvs.ctx.fillRect(569, 51.5, 150, 37.5)
+		cvs.ctx.fillStyle = 'rgb(200, 100, 200)'
+		cvs.ctx.fillRect(569, 89, 150, 37.5)
+		cvs.ctx.fillStyle = 'rgb(250, 150, 150)'
+		cvs.ctx.fillRect(569, 126.5, 150, 37.5)
+
+		for (let i = 0; i < 10; i++) {
+			// semi transparent to fully opaque white
+			// using rgb for opacity
+			cvs.ctx.fillStyle = `rgb(255 255 255 / ${(i + 1) / 10})`
+			for (let j = 0; j < 4; j++) {
+				cvs.ctx.fillRect((5 + i * 14) + 569, (5 + j * 37.5) + 14, 14, 27.5)
+			}
+		}
 	},
 
 	colors: false,
