429de09c8d6f6b1ab0f1f7824c5e0aa6ae2183ed
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-04-11 22:27:26 -0500

    add default enable all toggle to menus

--- src/js/modules/menuMgmt.mjs
+++ src/js/modules/menuMgmt.mjs
@@ -4,10 +4,21 @@
 
 import menu from './menu.mjs'
 
+async function addDefaultEnabledAll(menu) {
+	// create id for menu item
+	const id = `enable-all-${menu.id}`
+	// pass callback as lambda to not lose the context on 'this'
+	await menu.addMenuItem(id, 'enable all', `${id}-check`, (args) => menu.enableAllCallback(args))
+}
+
 const menuMgmt = {
 	createMenu: async function (menuId) {
+		// create menu
 		const newMenu = menu(menuId)
 		await newMenu.init()
+		// add default enable all menu item
+		await addDefaultEnabledAll(newMenu)
+		// return new menu
 		return newMenu
 	}
 }
