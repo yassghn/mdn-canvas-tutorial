b5149baed79d37ae8b87e95d1e4ea28bef46d038
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-04-15 19:36:58 -0500

    create genereric load image function, load new resource

--- src/js/modules/loadWebResource.mjs
+++ src/js/modules/loadWebResource.mjs
@@ -2,12 +2,22 @@
  * loadWebResource.mjs
  */
 
-export async function loadPatternImage() {
+async function loadImage(src, id) {
 	const img = new Image()
-	img.src = '/resource/pattern-mdn-tutorial.png'
-	img.setAttribute('id', 'patterns-image')
+	img.src = src
+	img.setAttribute('id', id)
 	img.setAttribute('hidden', '')
 	// await decode to ensure image is loaded
 	await img.decode()
 	document.body.appendChild(img)
+}
+
+export async function loadPatternImage() {
+	// load image
+	await loadImage('/resource/pattern-mdn-tutorial.png', 'patterns-image')
+}
+
+export async function loadBackdropImage() {
+	// load image
+	await loadImage('/resource/backdrop.png', 'drawing-images-backdrop-image')
 }
