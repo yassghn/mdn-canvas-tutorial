0127a0d961841cae9aeb2a68d33ca838183736c8
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-04-20 17:14:00 -0500

    give complex pallete objects functionality to clear after every draw

--- src/js/modules/complexPallete.mjs
+++ src/js/modules/complexPallete.mjs
@@ -2,17 +2,21 @@
  * complexPallete.mjs
  */
 
-function getComplexePallete(xpos, ypos, drawFunc, clearFunc) {
+function getComplexePallete(xpos, ypos, drawFunc, clearFunc, clear = false) {
 	const complexPallete = {
 		x: xpos,
 		y: ypos,
 		cleared: true,
+		clearEveryDraw: clear,
 		drawCallback: drawFunc,
 		clearCallback: clearFunc,
 
 		draw: function (enabled, cvs, previousTimestamp, timestamp) {
 			if (enabled) {
 				this.cleared = false
+				if (this.clearEveryDraw) {
+					this.clearCallback(this.x, this.y, cvs)
+				}
 				this.drawCallback(cvs, previousTimestamp, timestamp)
 			} else {
 				if (!this.cleared) {
