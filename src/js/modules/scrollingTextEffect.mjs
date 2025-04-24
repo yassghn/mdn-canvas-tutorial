26b799f08dbe44f95e31b5256de31dbd5f5051ce
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-04-24 03:00:57 -0500

    module for scrolling text effect properties

---
+++ src/js/modules/scrollingTextEffect.mjs
@@ -0,0 +1,55 @@
+/**
+ * scrollingTextEffect.mjs
+ */
+
+function measureText(ctxState, text) {
+	const width = ctxState.apply((ctx, text) => {
+		return ctx.measureText(text).width
+	}, text)
+	return width
+}
+
+function setProps(effect, ctxState, text, maxWidth, startXpos, ypos, scrollWidth) {
+	// set properties
+	effect.props.text = text
+	effect.props.textWidth = measureText(ctxState, text)
+	effect.props.maxWidth = maxWidth
+	effect.props.startX = startXpos
+	effect.props.x = startXpos
+	effect.props.y = ypos
+	effect.props.scrollWidth = scrollWidth
+}
+
+function updateProps(props) {
+	// scroll to the left
+	props.x -= props.scrollWidth
+	// check for reset x
+	if (Math.abs(props.x - props.startX) > props.textWidth + Math.abs(props.startX) + (props.scrollWidth * 30)) {
+		props.x = Math.abs(props.startX) + (props.scrollWidth * 30)
+	}
+}
+
+function scrollingTextEffect(...props) {
+	// create effect
+	const effect = {
+		props: {
+			text: '',
+			textWidth: 0,
+			maxWidth: 0,
+			startX: 0,
+			x: 0,
+			y: 0,
+			scrollWidth: 0
+		},
+
+		update: function () {
+			updateProps(this.props)
+		}
+	}
+	// set effects initial properties
+	setProps(effect, ...props)
+	// return effect
+	return effect
+}
+
+export default scrollingTextEffect
