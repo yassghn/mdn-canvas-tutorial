a1ba34f89758424a97ba5b97a039e57831755354
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-04-23 23:10:30 -0500

    module to apply canvas context properties to a callback

---
+++ src/js/modules/ContextState.mjs
@@ -0,0 +1,40 @@
+/**
+ * contextState.mjs
+ */
+
+import ContextProperties from "./ContextProperties.mjs"
+
+function ContextState(ctx, properties) {
+	this.self = undefined
+	this.ctx = undefined
+	this.props = undefined
+
+	const init = (ctx, properties) => {
+		if (!this.self) {
+			this.self = this
+			this.ctx = ctx
+			this.props = new ContextProperties(properties)
+		}
+	}
+
+	const setProps = (self) => {
+		self.props.set(self.ctx)
+	}
+
+	this.apply = (lambda, ...args) => {
+		// save state
+		this.ctx.save()
+		// set canvas state properties
+		setProps(this.self)
+		// get return value of lambda (if any)
+		const ret = lambda(this.ctx, ...args)
+		// restore state
+		this.ctx.restore()
+		// return value
+		return ret
+	}
+
+	init(ctx, properties)
+}
+
+export default ContextState
