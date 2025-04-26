f4dd2d75c1c57be081fdced112af6912daeeb349
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-04-25 21:55:46 -0500

    peripheral input module to manage keyboard and mouse events

---
+++ src/js/modules/peripheralInput.mjs
@@ -0,0 +1,60 @@
+/**
+ * peripheralInput.mjs
+ */
+
+import settings from './settings.mjs'
+
+const _keyBindings = {
+	modifier: {
+		ctrl: true
+	},
+
+	keys: {
+		enter: {
+			key: 'Enter',
+			keyCode: 13
+		}
+	}
+}
+
+function isTargetKey(event) {
+	// check for enter key
+	const enter = _keyBindings.keys.enter
+	if (event.key === enter.key && event.keyCode == enter.keyCode) {
+		return true
+	}
+}
+
+function isValidModifier(event) {
+	if (_keyBindings.modifier.ctrl) {
+		return event.ctrlKey
+	}
+}
+
+function processInput(event) {
+	switch (event.key) {
+		case _keyBindings.keys.enter.key:
+			settings.drawGridLines = settings.drawGridLines == true ? false : true
+			break;
+	}
+}
+
+function _addKeyboardListener() {
+	document.addEventListener('keydown', (event) => {
+		// check if pre configured modifier was used
+		if (isValidModifier(event)) {
+			// check if key combo is valid
+			if (isTargetKey(event)) {
+				processInput(event)
+			}
+		}
+	})
+}
+
+const peripheralInput = {
+	init: function () {
+		_addKeyboardListener()
+	}
+}
+
+export default peripheralInput
