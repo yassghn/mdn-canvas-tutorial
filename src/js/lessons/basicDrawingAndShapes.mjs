0d67eab6bebfbae0257597c21ca562a530eb698f
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-05-05 23:47:33 -0500

    kill them all. let 'em burn!

--- src/js/lessons/basicDrawingAndShapes.mjs
+++ src/js/lessons/basicDrawingAndShapes.mjs
@@ -202,7 +202,7 @@ const basicDrawingAndShapes = {
 		// animate quote bubble text
 		const dx = .99
 		const bubbleWidth = 100
-		const text = 'TODAYS NEWS @7: yassghn. man? or machine?'
+		const text = 'TODAYS NEWS @7: yassghn. man? machine? manchine?!'
 		const props = new ContextProperties()
 		props.font = '36px tahoma'
 		props.lineWidth = 1
