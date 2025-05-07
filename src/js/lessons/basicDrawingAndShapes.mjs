abf1e434b4bb0c55c48aa3403e782ee8ae79c0fe
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-05-06 23:57:54 -0500

    you have...1...new messages

--- src/js/lessons/basicDrawingAndShapes.mjs
+++ src/js/lessons/basicDrawingAndShapes.mjs
@@ -202,7 +202,7 @@ const basicDrawingAndShapes = {
 		// animate quote bubble text
 		const dx = .99
 		const bubbleWidth = 100
-		const text = 'TODAYS NEWS @7: cook them yassghn. cook them good. cook them for us, through us. just make sure they known they been cooked. 07'
+		const text = 'TODAYS NEWS @7: (CHOP)...(screams)...dawaj łapy...dawaj, albo odetnę Ci głowę...dawaj łapy!'
 		const props = new ContextProperties()
 		props.font = '36px tahoma'
 		props.lineWidth = 1
