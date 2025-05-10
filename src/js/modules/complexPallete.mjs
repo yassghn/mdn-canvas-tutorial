ea9d822d76cacedb5557eaf5c28e68ec43b552c9
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-05-10 14:33:14 -0500

    gut complex pallete object creation

--- src/js/modules/complexPallete.mjs
+++ src/js/modules/complexPallete.mjs
@@ -2,27 +2,13 @@
  * complexPallete.mjs
  */
 
-function getComplexePallete(xpos, ypos, drawFunc, clearFunc, clear = false) {
+function getComplexePallete(drawFunc) {
 	const complexPallete = {
-		x: xpos,
-		y: ypos,
-		cleared: true,
-		clearEveryDraw: clear,
 		drawCallback: drawFunc,
-		clearCallback: clearFunc,
 
 		render: function (enabled, cvs, ...args) {
 			if (enabled) {
-				this.cleared = false
-				if (this.clearEveryDraw) {
-					this.clearCallback(this.x, this.y, cvs)
-				}
 				this.drawCallback(cvs, ...args)
-			} else {
-				if (!this.cleared) {
-					this.cleared = true
-					this.clearCallback(this.x, this.y, cvs)
-				}
 			}
 		}
 	}
