a6afcd3425cb16e841566c24fca9765aaec36b34
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-05-10 21:12:21 -0500

    pallete builder with interface

--- src/js/modules/palleteHewer.mjs
+++ src/js/modules/palleteHewer.mjs
@@ -22,53 +22,140 @@ const _palletes = {
 	accelerationPallete: null
 }
 
+/**
+ * @typedef _palleteStruct
+ * @type {object} pallete structure
+ * @property {function} render render function
+ * @property {boolean} complex complex pallete flag
+ * @property {object} vars object containing pallete specific variables
+ * @property {object[]} args rest/spread args for render
+ */
+const _palleteStruct = {
+	render: null,
+	complex: false,
+	vars: null,
+	args: []
+}
+
+/**
+ * 
+ * @param {_palleteStruct} palleteStruct
+ * @returns initialized pallete object
+ */
+function buildPallete(palleteStruct) {
+	// init return
+	let pallete = null
+	// init struct: set default values, then overwrite with param
+	const struct = { ..._palleteStruct, ...palleteStruct }
+
+	// complex pallete
+	if (palleteStruct.complex) {
+		// build complex pallete
+		pallete = hewComplexePallete(struct.render, ...struct.args)
+		// check if we're setting vars
+		if (struct.vars) {
+			pallete.varsObj = { ...struct.vars }
+		}
+	} else {
+		// non complex pallete
+	}
+
+	return pallete
+}
+
 function _initPalletes() {
+	// init return
 	const palletes = { ..._palletes }
+
 	// init effects
 	const efx = effects()
 	const neonGlitch = efx.text.neonGlitch()
 	const scrollLeft = efx.text.leftScroll()
 	const mouseFollowParticles = efx.particles.spinningParticles()
+
 	// bezier and quadratic curves
-	palletes.bezierAndQuadraticCurvesPallete = hewComplexePallete(basicDrawingAndShapes.drawBezierAndQuadraticCurves, scrollLeft)
+	palletes.bezierAndQuadraticCurvesPallete = buildPallete({
+		render: basicDrawingAndShapes.drawBezierAndQuadraticCurves,
+		complex: true,
+		args: [scrollLeft]
+	})
+
 	// line styles
-	palletes.lineStylesPallete = hewComplexePallete(stylesColorsAndText.drawLineStyles)
-	palletes.lineStylesPallete.varsObj = {
-		delay: 15,
-		lineDashOffset: 0,
-		lineStylesLastDraw: 0
-	}
+	palletes.lineStylesPallete = buildPallete({
+		render: stylesColorsAndText.drawLineStyles,
+		complex: true,
+		vars: {
+			delay: 15,
+			lineDashOffset: 0,
+			lineStylesLastDraw: 0
+		}
+	})
+
 	// shadows
-	palletes.shadowsPallete = hewComplexePallete(stylesColorsAndText.drawShadows, neonGlitch)
-	palletes.shadowsPallete.varsObj = {
-		shadowDelay: 122,
-		shadowsLastDraw: 0
-	}
+	palletes.shadowsPallete = buildPallete({
+		render: stylesColorsAndText.drawShadows,
+		complex: true,
+		vars: {
+			shadowDelay: 122,
+			shadowsLastDraw: 0
+		},
+		args: [neonGlitch]
+	})
+
 	// clipping paths
-	palletes.clippingPathsPallete = hewComplexePallete(clippingAndAnimations.drawClippingPaths)
+	palletes.clippingPathsPallete = buildPallete({
+		render: clippingAndAnimations.drawClippingPaths,
+		complex: true
+	})
+
 	// inverse clipping paths
-	palletes.inverseClippingPathsPallete = hewComplexePallete(clippingAndAnimations.drawInverseClippingPaths)
+	palletes.inverseClippingPathsPallete = buildPallete({
+		render: clippingAndAnimations.drawInverseClippingPaths,
+		complex: true
+	})
+
 	// looping panorama
-	palletes.loopingPanoramaPallete = hewComplexePallete(clippingAndAnimations.drawLoopingPanorama)
-	palletes.loopingPanoramaPallete.varsObj = {
-		loopingPanoramaTimestamp: 0,
-		loopingPanoramaImageX: 0
-	}
+	palletes.loopingPanoramaPallete = buildPallete({
+		render: clippingAndAnimations.drawLoopingPanorama,
+		complex: true,
+		vars: {
+			loopingPanoramaTimestamp: 0,
+			loopingPanoramaImageX: 0
+		}
+	})
+
 	// clock
-	palletes.clockPallete = hewComplexePallete(clippingAndAnimations.drawClock)
+	palletes.clockPallete = buildPallete({
+		render: clippingAndAnimations.drawClock,
+		complex: true
+	})
+
 	// mouse follow
-	palletes.mouseFollowingPallete = hewComplexePallete(clippingAndAnimations.drawMouseFollowing, mouseFollowParticles)
+	palletes.mouseFollowingPallete = buildPallete({
+		render: clippingAndAnimations.drawMouseFollowing,
+		complex: true,
+		args: [mouseFollowParticles]
+	})
+
 	// boundaries
-	palletes.boundariesPallete = hewComplexePallete(clippingAndAnimations.drawBoundaries)
-	palletes.boundariesPallete.varsObj = {
-		sword: getSword()
-	}
+	palletes.boundariesPallete = buildPallete({
+		render: clippingAndAnimations.drawBoundaries,
+		complex: true,
+		vars: {
+			sword: getSword()
+		}
+	})
+
 	// acceleration
-	palletes.accelerationPallete = hewComplexePallete(clippingAndAnimations.drawAcceleration)
-	palletes.accelerationPallete.varsObj = {
-		ball: getBall(),
-		ballTrail: effects().shapes.ballTrail(),
-	}
+	palletes.accelerationPallete = buildPallete({
+		render: clippingAndAnimations.drawAcceleration,
+		complex: true,
+		vars: {
+			ball: getBall(),
+			ballTrail: effects().shapes.ballTrail(),
+		}
+	})
+
 	return palletes
 }
 
