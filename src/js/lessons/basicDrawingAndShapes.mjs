cf43c038089c6f94c9371f0eaa6e1b46044a77ba
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-05-13 19:19:33 -0500

    i see you...

--- src/js/lessons/basicDrawingAndShapes.mjs
+++ src/js/lessons/basicDrawingAndShapes.mjs
@@ -202,7 +202,7 @@ const basicDrawingAndShapes = {
 		// animate quote bubble text
 		const dx = .99
 		const bubbleWidth = 100
-		const text = 'TODAYS NEWS @7: remember, my friends! through, with, within ether context properties patterns overlay, this, OUI is defined!'
+		const text = 'TODAYS NEWS @7: the next set of examples are up on our chopping block! chop \'em!'
 		const props = new ContextProperties()
 		props.font = '36px tahoma'
 		props.lineWidth = 1
