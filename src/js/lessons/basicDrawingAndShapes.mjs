a5158590bf36c2bcd72e745ed1f3c210d5a09c68
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-05-21 15:27:09 -0500

    the CLEANSING

--- src/js/lessons/basicDrawingAndShapes.mjs
+++ src/js/lessons/basicDrawingAndShapes.mjs
@@ -202,7 +202,7 @@ const basicDrawingAndShapes = {
 		// animate quote bubble text
 		const dx = .99
 		const bubbleWidth = 100
-		const text = 'TODAYS NEWS @7: you chose a brain dead rat as a leader and then blamed me for defeating a teency little rat. dumb as rocks. lol'
+		const text = 'TODAYS NEWS @7: you came to believe all the lies you told about me. no one will miss the little teency hitlerites with less then 0 accomplishments.'
 		const props = new ContextProperties()
 		props.font = '36px tahoma'
 		props.lineWidth = 1
