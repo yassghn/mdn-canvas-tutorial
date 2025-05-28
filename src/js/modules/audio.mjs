d71ac814fc444ccffcaeb676af3da59034501118
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-05-27 20:33:54 -0500

    surprise guest module

---
+++ src/js/modules/audio.mjs
@@ -0,0 +1,105 @@
+/**
+ * audio.mjs
+ */
+
+import { config } from './config.mjs'
+import audioData from '../../../resource/data/audio.json' with { type: 'json' }
+
+const _audioState = {
+    isPlaying: false
+}
+
+function _hewAudioContext() {
+    const ctx = new AudioContext({ muted: true })
+    return ctx
+}
+
+function _hewAudioSource(audioCtx) {
+    const audioSrc = audioCtx.createBufferSource()
+    audioSrc.connect(audioCtx.destination)
+    return audioSrc
+}
+
+function _startAudio(audioSrc, audioData) {
+    audioSrc.buffer = audioData
+    audioSrc.start(0)
+    audioSrc.loop = true
+    _audioState.isPlaying = true
+}
+
+function _audioDataDecodeError(error) {
+    if (config.debug) {
+        console.error(error)
+    }
+}
+
+async function _decodeAudioData(audioData) {
+    // audio audio context and source
+    const audioCtx = _hewAudioContext()
+    const audioSrc = _hewAudioSource(audioCtx)
+    // decode audio data
+    try {
+        await audioCtx.decodeAudioData(
+            // array buffer
+            audioData,
+            // success callback
+            (audioData) => {
+                _startAudio(audioSrc, audioData)
+            },
+            // error callback
+            (error) => {
+                _audioDataDecodeError(error)
+            }
+        )
+    } catch (e) {
+        if (config.debug) {
+            console.log('audio decode error: check file, file name, missing file, etc.')
+        }
+    }
+}
+
+function _requestAudioData() {
+    const request = new XMLHttpRequest()
+    request.open('GET', audioData.audio.current, true)
+    request.responseType = 'arraybuffer'
+    request.onload = () => {
+        _decodeAudioData(request.response)
+    }
+    request.send()
+}
+
+function _validateRequestAudio() {
+    // check if we're already playing audio
+    if (!_audioState.isPlaying) {
+        try {
+            // init audio
+            _requestAudioData()
+        } catch (e) {
+            console.error(e)
+        }
+    }
+}
+
+function _processClick(event) {
+    event.preventDefault()
+    // check for mouse click
+    if (event.buttons != undefined) {
+        _validateRequestAudio()
+    }
+}
+
+function _addDocumentClickListener() {
+    document.addEventListener('click', _processClick)
+}
+
+function _playAudio() {
+    _addDocumentClickListener()
+}
+
+const audio = {
+    playAudio: () => {
+        _playAudio()
+    }
+}
+
+export default audio
