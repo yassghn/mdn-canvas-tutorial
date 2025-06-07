dd899cdb14e1222e65126cb0927c78cb8f5ddc8a
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-07 14:50:02 -0500

    genesis 18:20 esv, again

--- src/js/lessons/basicDrawingAndShapes.mjs
+++ src/js/lessons/basicDrawingAndShapes.mjs
@@ -202,7 +202,7 @@ const basicDrawingAndShapes = {
 		// animate quote bubble text
 		const dx = .99
 		const bubbleWidth = 100
-		const text = 'TODAYS NEWS @7: transition to assault preparations phase theta'
+		const text = 'TODAYS NEWS @7: bring the kings of filth & rats to their knees. return their abominations to dust; annihilate them. overthrow them. let them see with their own eyes what happened in sodom, gomorrah, admah, zeboiim, and bela. such is the cost of their treason.'
 		const props = new ContextProperties()
 		props.font = '36px tahoma'
 		props.lineWidth = 1
