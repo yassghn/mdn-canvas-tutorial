1949c10bae102da4e9ae7497b18c2882d85af812
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-26 19:45:12 -0500

    cowardly little brain dead rats

--- src/js/lessons/basicDrawingAndShapes.mjs
+++ src/js/lessons/basicDrawingAndShapes.mjs
@@ -202,7 +202,7 @@ const basicDrawingAndShapes = {
 		// animate quote bubble text
 		const dx = .99
 		const bubbleWidth = 100
-		const text = 'TODAYS NEWS @7: your little tiny rat brain then thought what? you were finally going to win and defeat me for the empire? GodDAMMNED brain dead rat.'
+		const text = 'TODAYS NEWS @7: your little tiny rat brain then thought what? you were finally going to win and defeat me for the empire? with what?! more lies, garbage, and toys?! these things you think are wunderwaffen?! typical american. GodDAMMNED brain dead rat.'
 		const props = new ContextProperties()
 		props.font = '36px tahoma'
 		props.lineWidth = 1
